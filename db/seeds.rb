1460bda17361d7bf10517c6f3cca929b82be9194
Kanda Mateta <matetah@gmail.com>
2020-03-25 15:21:51 -0400

    added routes and link user to team

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# create 3 teams...
+3.times do
+    team = Team.create(name: Faker::Team.name, sport: Faker::Team.sport,)
+    # ...with 5 users each
+    5.times do
+      team.users.create(name: Faker::Name.unique.name,
+        # no need to seed team_id, it'll do it for you. MAGIC!
+      )
+    end
+  end  
